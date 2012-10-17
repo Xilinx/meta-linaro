088a998c688fa67dc2e7fa347a65a339ee1cc575
Fathi Boudra <fathi.boudra@linaro.org>
2012-10-17 14:32:35 +0300

    Skip package installation if WORKSPACE is defined (e.g on Jenkins).

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,9 @@
 
 # install required packages
 
-sudo apt-get install -y --force-yes sed wget cvs subversion git bzr coreutils unzip bzip2 tar gzip cpio gawk python patch diffstat make build-essential gcc g++ desktop-file-utils chrpath autoconf automake libgl1-mesa-dev libglu1-mesa-dev libsdl1.2-dev texi2html texinfo realpath
+if [ -z "${WORKSPACE}" ]; then
+  sudo apt-get install -y --force-yes sed wget cvs subversion git bzr coreutils unzip bzip2 tar gzip cpio gawk python patch diffstat make build-essential gcc g++ desktop-file-utils chrpath autoconf automake libgl1-mesa-dev libglu1-mesa-dev libsdl1.2-dev texi2html texinfo realpath
+fi
 
 # clone repositories
 
