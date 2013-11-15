8aa1a29d70db7d7de635c525e1134df6863e3bb3
Tyler Baker <tyler.baker@linaro.org>
2013-11-15 15:48:08 -0800

    linaro-image-minimal-initramfs: use devtmpfs over mknod to increase portability
    Change-Id: I9b215b779d8273027d8c950f85d6955186ae1c8a
    Signed-off-by: Tyler Baker <tyler.baker@linaro.org>

--- meta-linaro/recipes-core/initrdscripts/files/init-boot-linaro.sh
+++ meta-linaro/recipes-core/initrdscripts/files/init-boot-linaro.sh
@@ -6,20 +6,12 @@ PS1="linaro-test [rc=$(echo \$?)]# "
 export HOME PS1 PATH
 
 early_setup() {
-    mkdir /proc /sys /tmp /run
+    mkdir -p /proc /sys /tmp /run
     mount -t proc proc /proc
     mount -t sysfs sysfs /sys
+    mount -t devtmpfs none /dev
 
     ln -s /run /var/run
-
-    # ARM AMBA SoCs
-    mknod /dev/ttyAMA0 c 204 64
-    mknod /dev/ttyAMA2 c 204 66
-    # Samsung ARM SoCs
-    mknod /dev/ttySAC2 c 204 66
-    # TI OMAP SoCs
-    mknod /dev/ttyO0 c 249 0
-    mknod /dev/ttyO2 c 249 2
 
     chmod 0666 /dev/tty*
     chown root:tty /dev/tty*
